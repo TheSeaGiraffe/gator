6509af25fb1fd69d38ab9cd5cb72b7961e927543
Fahmi Adi Nugraha <fnugraha@gmail.com>
2024-10-07 11:06:54 +0700

    Add register subcommand for adding a new user to the database. Update login subcommand to first check if a user exists before logging them in.

--- internal/commands/handlers.go
+++ internal/commands/handlers.go
@@ -1,11 +1,19 @@
 package commands
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"time"
 
+	"github.com/TheSeaGiraffe/gator/internal/database"
 	"github.com/TheSeaGiraffe/gator/internal/state"
+	"github.com/google/uuid"
 )
 
+// HandlerLogin is a handler for the `login` subcommand. `login` is used to set the current user
+// to the specified user.
 func HandlerLogin(s *state.State, cmd Command) error {
 	if len(cmd.Args) == 0 {
 		return fmt.Errorf("No username provided")
@@ -13,11 +21,66 @@ func HandlerLogin(s *state.State, cmd Command) error {
 		return fmt.Errorf("Username must be a single string")
 	}
 
-	err := s.Config.SetUser(cmd.Args[0])
+	user, err := s.DB.GetUser(context.Background(), cmd.Args[0])
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return fmt.Errorf("User '%s' does not exist", cmd.Args[0])
+		default:
+			return err
+		}
+	}
+
+	err = s.Config.SetUser(user.Name)
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("Username has been set to '%s'\n", s.Config.CurrentUserName)
+
+	return nil
+}
+
+// HandlerRegister is a handler for the `register` subcommand. `register` adds the current user
+// to the database.
+func HandlerRegister(s *state.State, cmd Command) error {
+	// Check that a username was passed in the args
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("No username provided")
+	} else if len(cmd.Args) > 1 {
+		return fmt.Errorf("Username must be a single string")
+	}
+
+	// Create a new user in the database
+	userData := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.Args[0],
+	}
+
+	user, err := s.DB.CreateUser(context.Background(), userData)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_name_key"`:
+			return fmt.Errorf("User '%s' already exists", cmd.Args[0])
+		default:
+			return err
+		}
+	}
+
+	// Set the current user in the config to the given name
+	err = s.Config.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User '%s' successfully created\n\n", user.Name)
+	fmt.Printf("User info:\n\n")
+	fmt.Printf("Name: %s\n", user.Name)
+	fmt.Printf("ID: %s\n", user.ID)
+	fmt.Printf("Created At: %s\n", user.CreatedAt.String())
+	fmt.Printf("Updated At: %s\n", user.UpdatedAt.String())
+
 	return nil
 }
--- main.go
+++ main.go
@@ -19,9 +19,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Maybe combine setting up the State struct into a single function later
 	db, err := sql.Open("postgres", cfg.DBUrl)
 	if err != nil {
 		fmt.Printf("Error connecting to database: %s", err.Error())
+		os.Exit(1)
 	}
 	dbQueries := database.New(db)
 
@@ -30,9 +32,12 @@ func main() {
 		Config: cfg,
 	}
 
+	// Maybe combine the command registration logic into a single function later
 	cmds := commands.InitCommands()
 	cmds.Register("login", commands.HandlerLogin)
+	cmds.Register("register", commands.HandlerRegister)
 
+	// Maybe combine the logic for running commands into a single function
 	userArgs := os.Args
 	if len(userArgs) < 2 {
 		fmt.Println("Not enough arguments")
@@ -45,18 +50,14 @@ func main() {
 		cmdArgs = userArgs[2:]
 	}
 
-	loginCmd := commands.Command{
+	cmd := commands.Command{
 		Name: cmdName,
 		Args: cmdArgs,
 	}
 
-	err = cmds.Run(&st, loginCmd)
+	err = cmds.Run(&st, cmd)
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
 		os.Exit(1)
 	}
-
-	fmt.Println("Successfully loaded config file. Current config:")
-	fmt.Printf("\nDB URL: '%s'\n", cfg.DBUrl)
-	fmt.Printf("Name of current user: '%s'\n", cfg.CurrentUserName)
 }
